0c62dc572ceebbfaf949b66a56db904cb96f13ec
Bob Robey <brobey@lanl.gov>
2020-07-25 19:17:56 -0600

    Fixing cloverleaf number of cpus

--- CloverLeaf_Skylake_batch.sh
+++ CloverLeaf_Skylake_batch.sh
@@ -7,11 +7,16 @@ if [ `which icc |wc -l` = 1 ]; then
 else
   make COMPILER=GNU
 fi
+NUM_CPUS=36
+AVAIL_CPUS=`grep '^CPUS' | cut -d'\t' -f1`
+if [ ${AVAIL_CPUS} -lt ${NUM_CPUS} ]; then
+   NUM_CPUS=${AVAIL_CPUS}
+fi
 
 PROB=clover_bm64
 
 sed -e '1,$s/end_step=2955/end_step=500/' InputDecks/$PROB.in > clover.in
-mpirun -n 36 --bind-to core ./clover_leaf 2>&1 | tee $PROB.out
+mpirun -n ${NUM_CPUS} --bind-to core ./clover_leaf 2>&1 | tee $PROB.out
 
-likwid-mpirun -np 36 -g MEM_DP ./clover_leaf 2>&1 | tee $PROB_likwid.out
+#likwid-mpirun -np ${NUM_CPUS} -g MEM_DP ./clover_leaf 2>&1 | tee $PROB_likwid.out
 
