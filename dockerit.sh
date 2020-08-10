a840fb66e7f458a58b48465c1e51844b17a939c4
Bob Robey <brobey@lanl.gov>
2020-08-10 15:41:55 -0600

    Container fixes

--- dockerit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -f Dockerfile.Ubuntu20.04 -t chapter9 .
-docker run -it --entrypoint /bin/bash chapter9
--- dockertest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker build --no-cache -t chapter9 .
-#docker run -it --entrypoint /bin/bash chapter9
-docker build --no-cache -f Dockerfile.Ubuntu20.04 -t chapter9 .
-#docker run -it --entrypoint /bin/bash chapter9
-docker build --no-cache -f Dockerfile.debian -t chapter9 .
-#docker run -it --entrypoint /bin/bash chapter9
