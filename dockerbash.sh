43d3e97ed166928765aaa53769131c7a9b399367
Bob Robey <brobey@lanl.gov>
2020-10-26 20:38:29 -0600

    Fixing container for MPI

--- dockerbash.sh
+++ dockerbash.sh
@@ -2,4 +2,5 @@
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 docker run -it --gpus all -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
+	--security-opt seccomp=unconfined --cap-add=SYS_PTRACE \
         --entrypoint /bin/bash essentialsofparallelcomputing/chapter9
